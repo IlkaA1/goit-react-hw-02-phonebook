9dd791dc6cded1f2a1e2ab6dfe03a58ae8b57536
Ilona Kamash <ilonka.kamash@gmail.com>
2023-06-12 11:40:06 +0200

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,9 @@
 const Contact = ({ getFilteredElement }) => {
-    return { getFilteredElement.map((contact) => (
-          <li key={contact.id}>
-            {contact.name}: {contact.number}
-          </li>
-        ))};
+  //     return { getFilteredElement.map((contact) => (
+  //           <li key={contact.id}>
+  //             {contact.name}: {contact.number}
+  //           </li>
+  //         ))};
 };
 
 export default Contact;
